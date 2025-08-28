5074bef9a8357e99efc2091387f7efc71f46d95d
darianmorat <yojhandariantoledomora@gmail.com>
2025-08-28 14:31:34 -0500

    fix(extras): update wezterm color descriptions

--- extras/wezterm/gruvdark-light.lua
+++ extras/wezterm/gruvdark-light.lua
@@ -14,20 +14,20 @@ local themes = {
          "#1B1B1B", -- black
          "#9F0202", -- red
          "#006C00", -- green
-         "#AE5F05", -- yellow
+         "#AE5F05", -- orange
          "#1E5A8B", -- blue
-         "#910E79", -- purple
-         "#008B7F", -- cyan
+         "#910E79", -- pink
+         "#008B7F", -- aqua
          "#111111", -- white
       },
       brights = {
          "#111111", -- brightBlack
          "#9F0202", -- brightRed
          "#006C00", -- brightGreen
-         "#AE5F05", -- brightYellow
+         "#AE5F05", -- brightOrange
          "#1E5A8B", -- brightBlue
-         "#910E79", -- brightPurple
-         "#008B7F", -- brightCyan
+         "#910E79", -- brightPink
+         "#008B7F", -- brightAqua
          "#111111", -- brightWhite
       },
    },
--- extras/wezterm/gruvdark.lua
+++ extras/wezterm/gruvdark.lua
@@ -14,20 +14,20 @@ local themes = {
          "#4F4F4F", -- black
          "#E16464", -- red
          "#72BA62", -- green
-         "#D19F66", -- yellow
+         "#D19F66", -- orange
          "#579DD4", -- blue
-         "#D159B6", -- purple
-         "#00A596", -- cyan
+         "#D159B6", -- pink
+         "#00A596", -- aqua
          "#D6CFC4", -- white
       },
       brights = {
          "#4F4F4F", -- brightBlack
          "#E16464", -- brightRed
          "#72BA62", -- brightGreen
-         "#D19F66", -- brightYellow
+         "#D19F66", -- brightOrange
          "#579DD4", -- brightBlue
-         "#D159B6", -- brightPurple
-         "#00A596", -- brightCyan
+         "#D159B6", -- brightPink
+         "#00A596", -- brightAqua
          "#D6CFC4", -- brightWhite
       },
    },
