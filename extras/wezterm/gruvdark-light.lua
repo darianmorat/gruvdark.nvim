ca7f89181f42e75f384510cbf73f45c8f8b4d2ff
darianmorat <yojhandariantoledomora@gmail.com>
2025-09-04 10:59:55 -0500

    fix(extras): wezterm colors refactoring

--- extras/wezterm/gruvdark-light.lua
+++ extras/wezterm/gruvdark-light.lua
@@ -1,36 +1,32 @@
-local themes = {
-   gruvdark_light = {
-      foreground = "#111111",
-      background = "#F7F5EA",
-      cursor_fg = "#EAE7E1",
-      cursor_bg = "#404040",
-      cursor_border = "#404040",
-      selection_fg = "#101010",
-      selection_bg = "#B0B0A6",
-      split = "#4F4F4F",
-      scrollbar_thumb = "#4F4F4F",
+return {
+   foreground = "#111111",
+   background = "#F7F5EA",
+   cursor_fg = "#EAE7E1",
+   cursor_bg = "#404040",
+   cursor_border = "#404040",
+   selection_fg = "#101010",
+   selection_bg = "#B0B0A6",
+   split = "#4F4F4F",
+   scrollbar_thumb = "#4F4F4F",
 
-      ansi = {
-         "#1B1B1B", -- black
-         "#9F0202", -- red
-         "#006C00", -- green
-         "#AE5F05", -- orange
-         "#1E5A8B", -- blue
-         "#910E79", -- pink
-         "#008B7F", -- aqua
-         "#111111", -- white
-      },
-      brights = {
-         "#111111", -- brightBlack
-         "#9F0202", -- brightRed
-         "#006C00", -- brightGreen
-         "#AE5F05", -- brightOrange
-         "#1E5A8B", -- brightBlue
-         "#910E79", -- brightPink
-         "#008B7F", -- brightAqua
-         "#111111", -- brightWhite
-      },
+   ansi = {
+      "#1B1B1B", -- black
+      "#9F0202", -- red
+      "#006C00", -- green
+      "#AE5F05", -- orange
+      "#1E5A8B", -- blue
+      "#910E79", -- pink
+      "#008B7F", -- aqua
+      "#111111", -- white
+   },
+   brights = {
+      "#111111", -- brightBlack
+      "#9F0202", -- brightRed
+      "#006C00", -- brightGreen
+      "#AE5F05", -- brightOrange
+      "#1E5A8B", -- brightBlue
+      "#910E79", -- brightPink
+      "#008B7F", -- brightAqua
+      "#111111", -- brightWhite
    },
 }
-
-return themes
